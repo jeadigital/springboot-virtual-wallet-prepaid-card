a09a6dea1db88d0606c4b3fe6a3a3a1fb50aa500
Jagadeesh <blockchian@jeadigital.com>
2024-11-15 18:23:55 +0530

    Initial commit

---
+++ src/test/java/com/jea/payments/banking/SpringbootVirtualWalletPrepaidCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jea.payments.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVirtualWalletPrepaidCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
